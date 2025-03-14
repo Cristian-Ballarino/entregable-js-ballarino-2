96cde75849523c451f72ef22facba7f600910084
cristian ballarino <cristianballarino1@gmail.com>
2025-03-14 11:44:41 -0300

    avance proyecto

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const shopContent = document.getElementById("shopContent");
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Skate1",
+        precio: 98000,
+        img: "imagenes/2b951c4f-a80e-44fa-81bc-97783e56c9a2.jpg",
+    },
+    {
+        id: 2,
+        nombre: "Skate2",
+        precio: 98000,
+        img: "imagenes/a8ed8407-a7c6-4893-9f8b-e2d0bff0ac85.jpg",
+    },
+    {
+        
+        id: 3,
+        nombre: "Skate3",
+        precio: 98000,
+        img: "imagenes/Flashback 8_ - Complete Skateboard.jpg",
+    },
+    {
+        
+        id: 4,
+        nombre: "Skate4",
+        precio: 98000,
+        img: "imagenes/Skate semi profissional e iniciante hypeboards homem aranha até 100kg.jpg",
+    },
+];
+
+let carrito = [];
+
+productos.forEach((product)=> {
+    let content = document.createElement("div");
+    content.innerHTML = `
+      <img src="${product.img}">
+      <h3>${product.nombre}</h3>
+      <p>${product.precio} $</p>
+     `;
+
+shopContent.append(content);
+});
+``
